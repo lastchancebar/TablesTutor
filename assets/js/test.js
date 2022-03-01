bc8cc2d846a79760a459f6a987cb54f7450cd6f3
lastchancebar <darinaduffykelly@gmail.com>
2022-03-01 15:13:48 +0000

    add tables in js and some js on test question

--- assets/js/test.js
+++ assets/js/test.js
@@ -11,4 +11,83 @@ document.write("welcome to Javatpoint");
      document.write( '<p> + [tableoperand] tables.</p>');
      document.write('Congratulations!');
    }
+
+   const colors = [ '#ff0000', 'White', '#ff007f', '#7627ff', '#ffff00', '#000'];
    
+   function getTable(){
+     document.getElementById('playGround').innerHTML='';
+     let tableNumber = parseInt(document.getElementById('tableNumber').value);
+     applyLogic(tableNumber);
+   }
+   function applyLogic(tableNumber) { 
+     for (let counter= 0; counter<= 10; counter++) {
+       let addedValue= tableNumber + counter;
+       setTimeout(function() {
+         applyPlay(tableNumber + '+' + counter + '=' + addedValue);
+       }, 200 * counter);
+     }
+   }
+   function applyPlay(text){
+     let type= document.createElement('h2');
+     let random = Math.floor((Math.random() * colors.length));
+     type.style.color = colors[random];
+			type.style.fontSize = '50px';
+			type.style.fontWeight = '900';
+			type.style.margin = '0px';
+			type.style.marginRight = '50px';
+			type.style.padding = '0px';
+     type.appendChild(document.createTextNode(text));
+     document.getElementById('playGround').appendChild(type);
+
+   }
+   document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+                if (this.getAttribute("data-type") === "submit") {
+                    checkAnswer();
+                } else {
+                    let gameType = this.getAttribute("data-type");
+                    runGame(gameType);
+                }
+            
+            })
+            
+    }
+    document.getElementById("answer-box").addEventListener("keydown", function(event)
+{ if (event.key ==="Enter"){
+    checkAnswer();
+}
+
+})
+        runGame("addition");
+  })
+  
+/** 
+ * The game "loop", called when the script is loaded 
+ * and after the user's answer has been processed */
+function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
+    
+    let num1 = tableNumber;
+    let num2 = Math.floor(Math.random() * 10);
+
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+}else{
+  alert(`Unknown game type:${gameType}`);
+throw`Unknown game type:${gameType}. Aborting!`;/* does this need to be in?*/
+}
+}
+function checkAnswer(){
+  let userAnswer= parseInt(document.getElementById('answer-box').value);
+  let calculatedAnswer= calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
+
+  if (isCorrect){
+    
+  }
+}
